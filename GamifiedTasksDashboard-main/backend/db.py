a9aea9bcbbf765787fc55e5eac78bd8848fd6af3
Rahul Kulkarni <rahul.g.kulkarni@oracle.com>
2026-04-30 18:16:37 +0530

    Revert "Add Gamified Tasks Dashboard project"
    This reverts commit f2c7077e49f7da7f952535002029eec7c2de7697.

--- GamifiedTasksDashboard-main/backend/db.py
+++
@@ -1,8 +0,0 @@
-import oracledb, os
-
-def get_connection():
-    return oracledb.connect(
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        dsn=os.getenv("DB_DSN")
-    )
